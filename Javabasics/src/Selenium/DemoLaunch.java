f9ccfe475cade1b9ead2af5ee977675243b9d3fc
s <s@192.168.0.2>
2020-09-07 14:33:43 +0530

    Selinium

---
+++ Javabasics/src/Selenium/DemoLaunch.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoLaunch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",".\\Software\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		
+
+	}
+
+}
---
+++ Javabasics/src/Selenium/Hello.java
@@ -0,0 +1,10 @@
+package Selenium;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+
+	}
+
+}
